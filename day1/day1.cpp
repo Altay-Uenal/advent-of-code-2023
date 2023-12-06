dc3e4052bed39a4b6245797528753e8377004d0d
Altay Uenal <altayuenal@gmail.com>
2023-12-06 03:26:49 -0700

    finished day 1

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -21,6 +21,70 @@ int main(int argc, char* argv[]) {
             if (isdigit(line[i])) {
                 if (first == 0) first = line[i];
                 last = line[i];
+            } else {
+                switch (line[i]) {
+                    case 'o':
+                        if (line.substr(i, 3) == "one") {
+                            if (first == 0) first = '1';
+                            last = '1';
+                            i+=1;
+                        }
+                        break;
+
+                    case 't':
+                        if (line.substr(i, 3) == "two") {
+                            if (first == 0) first = '2';
+                            last = '2';
+                            i+=1;
+                        } else if (line.substr(i, 5) == "three") {
+                            if (first == 0) first = '3';
+                            last = '3';
+                            i+=3;
+                        }
+                        break;
+
+                    case 'f':
+                        if (line.substr(i, 4) == "four") {
+                            if (first == 0) first = '4';
+                            last = '4';
+                            i+=2;
+                        } else if (line.substr(i, 4) == "five") {
+                            if (first == 0) first = '5';
+                            last = '5';
+                            i+=2;
+                        }
+                        break;
+
+                    case 's':
+                        if (line.substr(i, 3) == "six") {
+                            if (first == 0) first = '6';
+                            last = '6';
+                            i+=1;
+                        } else if (line.substr(i, 5) == "seven") {
+                            if (first == 0) first = '7';
+                            last = '7';
+                            i+=3;
+                        }
+                        break;
+
+                    case 'e':
+                        if (line.substr(i, 5) == "eight") {
+                            if (first == 0) first = '8';
+                            last = '8';
+                            i+=3;
+                        }
+                        break;
+
+                    case 'n':
+                        if (line.substr(i, 4) == "nine") {
+                            if (first == 0) first = '9';
+                            last = '9';
+                            i+=2;
+                        }
+                        break;
+                    default: //do nothing
+                        break;
+                }
             }
         }
         sum += stoi(num+first+last);
